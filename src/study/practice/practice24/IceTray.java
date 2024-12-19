7a6fc300f276f5e8d1f33a6e71086d4540ad7cf1
YooUisun <yoouisun33@naver.com>
2024-12-19 16:57:20 +0900

    클래스 만들어보기 연습문제

---
+++ src/study/practice/practice24/IceTray.java
@@ -0,0 +1,7 @@
+package study.practice.practice24;
+
+public class IceTray {
+
+	int iceCount; //몇구?
+	int price; //가격?
+}
---
+++ src/study/practice/practice24/kitchen.java
@@ -0,0 +1,18 @@
+package study.practice.practice24;
+
+import java.util.ArrayList;
+
+public class Kitchen {
+	
+	int pyeong;
+	String wallColor;
+	int height;  //1200 x 600
+	
+	Refrigerator refri;
+	GasRange gasRange;
+	
+	Cup[] cups;
+	ArrayList<Cup> cupList;
+	
+	ArrayList<TrashCan> trashCanList;
+}
