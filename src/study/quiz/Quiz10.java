71b4e51ef9379e6753f566162dfabb5b718bd1b5
YooUisun <yoouisun33@naver.com>
2025-01-06 15:29:49 +0900

    퀴즈

--- src/study/quiz/Quiz10.java
+++ src/study/quiz/Quiz10.java
@@ -1,50 +1,134 @@
 package study.quiz;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
 public class Quiz10 {
 
-    private static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:orcl";
-    private static final String DB_USER = "scott";
-    private static final String DB_PASSWORD = "tiger";
+	public static void main(String[] args) {
 
-    public void findAllProfessors() {
-        String query = "SELECT profno, name, id, position, pay, deptno FROM professor";
-        try (
-            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            ResultSet resultSet = preparedStatement.executeQuery()
-        ) {
-            System.out.println("Professor List:");
-            boolean hasResults = false; // 결과 여부 체크
-            while (resultSet.next()) {
-                hasResults = true;
-                int profno = resultSet.getInt("profno");
-                String name = resultSet.getString("name");
-                String id = resultSet.getString("id");
-                String position = resultSet.getString("position");
-                int pay = resultSet.getInt("pay");
-                int deptno = resultSet.getInt("deptno");
-
-                System.out.printf("Profno: %d, Name: %s, ID: %s, Position: %s, Pay: %d, Deptno: %d%n",
-                        profno, name, id, position, pay, deptno);
-            }
-            if (!hasResults) {
-                System.out.println("No professors found.");
-            }
-        } catch (Exception e) {
-            System.err.println("Error retrieving data: " + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-
-
-    public static void main(String[] args) {
-        Quiz10 professorDAO = new Quiz10();
-        professorDAO.findAllProfessors(); // 전체 데이터 조회
-    }
-
+		EvenThread et = new EvenThread();
+		OddThread ot = new OddThread();
+		
+//		et.start();
+//		ot.start();
+		
+		EvenRunnable er = new EvenRunnable();
+		Thread t1 = new Thread(er);
+		Thread t2 = new Thread(new OddRunnable());
+		
+//		t1.start();
+//		t2.start();
+		
+		NumberThread nt1 = new NumberThread(true);
+		NumberThread nt2 = new NumberThread(false);
+		nt1.start();
+		nt2.start();
+	}
 }
+
+class NumberThread extends Thread {
+	
+	boolean isOdd;
+	int remain;
+	
+	public NumberThread(boolean isOdd) {
+		this.isOdd = isOdd;
+		if(isOdd) {
+			remain = 1;
+		} else {
+			remain = 0;
+		}
+	}
+	
+	public void run() {
+		
+		
+		
+		for(int i=1; i<=100; i++) {
+			if(i%2 == remain) {
+				System.out.println(i);
+			}
+//			if(isOdd) {
+//				if(i%2 == 1) {
+//					System.out.println(i);
+//				}
+//			} else {
+//				if(i%2 == 0) {
+//					System.out.println(i);
+//				}
+//			}
+			
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+}
+
+
+class EvenThread extends Thread {
+	public void run() {
+		for(int i=1; i<=100; i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+				
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
+	}
+}
+
+class OddThread extends Thread {
+	public void run() {
+		for(int i=1; i<=100; i+=2) {
+			System.out.println(i);
+			
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+
+class EvenRunnable implements Runnable {
+
+	public void run() {
+		for(int i=1; i<=100; i++) {
+			if(i%2 == 0) {
+				System.out.println(i);
+				
+				try {
+					Thread.sleep(200);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+			}
+		}
+	}
+	
+}
+
+class OddRunnable implements Runnable {
+	public void run() {
+		for(int i=1; i<=100; i+=2) {
+			System.out.println(i);
+			
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
